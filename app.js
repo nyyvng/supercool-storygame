aa74a32946459ade96279f2209d1bd47653edc10
Naylia <nylia.vang1@gmail.com>
2026-04-21 14:45:44 -0700

    im strugglingghfoidshngiore

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const btn = document.getElementById("btn");
-const image = document.getElementById("image");
+// const btn = document.getElementById("btn");
+// const image = document.getElementById("image");
 
-btn.addEventListener("click", () => {
-    image.src = "bg2.png";
+// btn.addEventListener("click", () => {
+//     image.src = "bg2.png";
 
-    const dialogs = document.querySelectorAll("dialouge");
-    dialogs[0].closest();
-    dialogs[1].showModal();
-}); //doesnt work for some reason :(
+//     const dialogs = document.querySelectorAll("dialouge");
+//     dialogs[0].closest();
+//     dialogs[1].showModal();
+
+// });              //doesnt work for some reason :(
